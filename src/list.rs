754968fc3679054cd37dc5c4c9364af3e50d8d62
rhysd <lin90162@yahoo.co.jp>
2017-05-14 21:04:40 +0900

    use Rc<Node<T>> wrapper for link of list

--- src/list.rs
+++ src/list.rs
@@ -7,18 +7,12 @@ use std::ops::Deref;
 #[derive(Clone, Debug)]
 pub enum Node<T: Clone + Debug> {
     Nil,
-    Cons(T, Link<T>),
+    Cons(T, List<T>),
 }
 
-type Link<T> = Rc<Node<T>>;
-
 // Just a wrapper of Rc<Node<T>> because `impl` cannot be used for external type `Rc`.
-// Below cannot compile.
-//
-// type List<T> = Rc<Node<T>>;
-// impl<T> List<T> { ... }
 #[derive(Clone, Debug)]
-pub struct List<T: Clone + Debug>(Link<T>);
+pub struct List<T: Clone + Debug>(Rc<Node<T>>);
 
 fn list<T: Clone + Debug>(n: Node<T>) -> List<T> {
     List(Rc::new(n))
@@ -39,7 +33,7 @@ where T: Clone + Debug {
     }
 
     pub fn cons(&self, v: T) -> Self {
-        list(Node::Cons(v, self.0.clone()))
+        list(Node::Cons(v, self.clone()))
     }
 
     pub fn head(&self) -> &T {
@@ -49,52 +43,40 @@ where T: Clone + Debug {
         }
     }
 
-    pub fn tail(&self) -> Self {
+    pub fn tail(&self) -> &Self {
         match *self.0 {
             Node::Nil => panic!("Node is empty!"),
-            Node::Cons(_, ref xs) => List(xs.clone()),
-        }
-    }
-
-    fn concat_impl(n: &Link<T>, other: &Self) -> Self {
-        match **n {
-            Node::Nil => other.clone(),
-            Node::Cons(ref x, ref xs) => List::concat_impl(xs, other).cons(x.clone()),
+            Node::Cons(_, ref xs) => xs,
         }
     }
 
     // Preceding list can be shared and don't need to be cloned.
     pub fn concat(&self, other: &List<T>) -> Self {
-        List::concat_impl(&self.0, other)
-    }
-
-    fn update_at_impl(n: &Link<T>, idx: u32, v: T) -> Self {
-        match **n {
-            Node::Nil => panic!("Node is empty!"),
-            Node::Cons(ref x, ref xs) =>
-                if idx == 0 {
-                    list(Node::Cons(v, xs.clone()))
-                } else {
-                    List::update_at_impl(n, idx - 1, v).cons(x.clone())
-                },
+        match *self.0 {
+            Node::Nil => other.clone(),
+            Node::Cons(ref x, ref xs) => xs.concat(other).cons(x.clone()),
         }
     }
 
     // Cells after `idx` can be shared (don't need to copy)
     pub fn update_at(&self, idx: u32, v: T) -> Self {
-        List::update_at_impl(&self.0, idx, v)
-    }
-
-    fn suffixes_impl(n: &Link<T>) -> List<Self> {
-        match **n {
-            Node::Nil => List::empty().cons(List::empty()),
-            Node::Cons(_, ref xs) => List::suffixes_impl(xs).cons(List(n.clone())),
+        match *self.0 {
+            Node::Nil => panic!("Node is empty!"),
+            Node::Cons(ref x, ref xs) =>
+                if idx == 0 {
+                    list(Node::Cons(v, xs.clone()))
+                } else {
+                    xs.update_at(idx-1, v).cons(x.clone())
+                },
         }
     }
 
     // Elements of returned list are shared.
     pub fn suffixes(&self) -> List<Self> {
-        List::suffixes_impl(&self.0)
+        match *self.0 {
+            Node::Nil => List::empty().cons(List::empty()),
+            Node::Cons(_, ref xs) => xs.suffixes().cons(self.clone()),
+        }
     }
 }
 
